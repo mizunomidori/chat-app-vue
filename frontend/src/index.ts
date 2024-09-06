c8d91c411a1f934c80380cbc52e2153fe75bac00
fanfoo <wtr.ssmt@gmail.com>
2024-09-07 02:51:26 +0900

    add packages

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 import { renderToString } from 'vue/server-renderer';
 import { createSSRApp } from 'vue';
 
@@ -11,12 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-console.log('env=', process.env.NODE_ENV);
+console.log('env =', process.env.NODE_ENV);
 // route
 app.use('/api/common', commonRouter);
 
 //routes
-app.get('/*', async (req: any, res: any) => {
+app.get('/*', async (req: Request, res: Response) => {
   const app = createSSRApp(App);
   const html = await renderToString(app, {});
   try {
@@ -26,9 +27,20 @@ app.get('/*', async (req: any, res: any) => {
   }
 });
 
-//start
+//routes
+app.get('/', async (req: Request, res: Response) => {
+  const oidcData = req.headers['x-amzn-oidc-data'];
+  if (oidcData && typeof oidcData === 'string') {
+    const payload = oidcData.split('.')[1];
+    // payloadをdecodeしてjson形式に変換
+    const decoded = JSON.parse(Buffer.from(payload, 'base64').toString('utf8'));
+    return { name: decoded.name ?? '', email: decoded.email ?? '' };
+  } else {
+    return { name: '', email: '' };
+  }
+});
+
 const PORT = 8080;
 app.listen({ port: PORT }, () => {
   console.log(`Server ready at http://localhost:${PORT}`);
 });
-console.log('start');
